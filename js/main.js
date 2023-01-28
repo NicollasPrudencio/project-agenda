fc221c195502ecbd6a57510dcf10020531fe07cb
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-01-28 14:38:33 -0300

    Projeto concluído

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    // Adiciona o efeito de deslizar para baixo no form
+    $('header button').click(function () {
+        $('form').slideDown();
+    })
+    // Adiciona o efeito de deslizar para cima no form
+    $('form #bt-cancel').click(function () {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const nameContact = $('#field-contact-name').val();
+        const numberContact = $('#field-contact-number').val();
+
+        const novoItem = $(`<tr></tr>`);
+        $(`
+
+        <th>${nameContact}</th>
+        <th>${numberContact}</th>
+        
+        `).appendTo(novoItem);
+
+
+        $(novoItem).appendTo('#tbody-table-contacts');
+        $(novoItem).fadeIn();
+    })
+
+
+})
